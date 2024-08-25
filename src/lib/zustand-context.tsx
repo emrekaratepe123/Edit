1baf559d75a764b6c9f1f32a8cde65c32633c08b
brunoDev100 <github_out1@outlook.com>
2024-08-25 14:12:47 +0530

    feat: Add zustand-context module for managing global state

---
+++ src/lib/zustand-context.tsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+import { StoreApi } from "zustand";
+
+export const createZustandContext = <TInitial, TStore extends StoreApi<any>>(
+  getStore: (initial: TInitial) => TStore
+) => {
+  const Context = createContext(null as any as TStore);
+  const Provider = (props: {
+    children?: React.ReactNode;
+    initialValue: TInitial;
+  }) => {
+    const [store] = useState(getStore(props.initialValue));
+    return <Context.Provider value={store}>{props.children}</Context.Provider>;
+  };
+
+  return {
+    useContext: () => useContext(Context),
+    Context,
+    Provider,
+  };
+};

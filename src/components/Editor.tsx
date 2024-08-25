a7cb36f7e0e4e11b34194be3d100efa69debc347
brunoDev100 <github_out1@outlook.com>
2024-08-25 18:02:19 +0530

    feat: Add ModeToggle and Layers components for theme and layer management

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 import UploadImage from "./upload/UploadImage";
+import Layers from "./layers/Layers";
+import { ModeToggle } from "./theme/ModeToggle";
 
 function Editor() {
   return (
-    <div>
+    <div className="flex h-full">
+      <div className="py-6 px-4 min-w-48 ">
+        <div className="pb-12 text-center">
+          <ModeToggle />
+        </div>
+      </div>
       <UploadImage />
+      <Layers />
     </div>
   );
 }

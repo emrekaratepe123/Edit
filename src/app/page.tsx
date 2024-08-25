bedccc50522edd5db039ee659166c26674fe4238
brunoDev100 <github_out1@outlook.com>
2024-08-25 18:02:45 +0530

    chore: Update page component to include LayerStore and ImageStore providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,31 @@
 
 import Editor from "@/components/Editor";
 import { ImageStore } from "@/lib/image-store";
+import { LayerStore } from "@/lib/layer-store";
 
 export default function Home() {
   return (
-    <ImageStore.Provider initialValue={{ generating: false }}>
-      <main>
-        <Editor />
-      </main>
-    </ImageStore.Provider>
+    <LayerStore.Provider
+      initialValue={{
+        layerComparisonMode: false,
+        layers: [
+          {
+            id: crypto.randomUUID(),
+            url: "",
+            height: 0,
+            width: 0,
+            publicId: "",
+          },
+        ],
+      }}
+    >
+      <ImageStore.Provider
+        initialValue={{ generating: false, activeTag: "all" }}
+      >
+        <main className="h-full">
+          <Editor />
+        </main>
+      </ImageStore.Provider>
+    </LayerStore.Provider>
   );
 }

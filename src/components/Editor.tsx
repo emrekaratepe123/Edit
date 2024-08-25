b0f4bde0fa75f85bd48861f5de315a0314bbcb93
brunoDev100 <github_out1@outlook.com>
2024-08-25 14:13:31 +0530

    feat: Add Editor component for uploading images

---
+++ src/components/Editor.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import UploadImage from "./upload/UploadImage";
+
+function Editor() {
+  return (
+    <div>
+      <UploadImage />
+    </div>
+  );
+}
+
+export default Editor;

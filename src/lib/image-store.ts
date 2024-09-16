f9705bf22bcdeb9d50d94dfc58a7aa4ee2bca7b9
brunoDev100 <github_out1@outlook.com>
2024-09-16 10:37:08 +0530

    refactor: Refactor image-store.ts to include tags and active color state

--- src/lib/image-store.ts
+++ src/lib/image-store.ts
@@ -6,14 +6,30 @@ import { useContext } from "react";
 type State = {
   generating: boolean;
   setGenerating: (generating: boolean) => void;
+  tags: string[];
+  setTags: (tags: string[]) => void;
+  activeTag: string;
+  setActiveTag: (tag: string) => void;
+  activeColor: string;
+  setActiveColor: (color: string) => void;
 };
 
-const getStore = (initialState: { generating: boolean }) => {
+const getStore = (initialState: {
+  generating: boolean;
+  activeTag: string;
+  activeColor: string;
+}) => {
   return createStore<State>()(
     persist(
       (set) => ({
         generating: initialState.generating,
         setGenerating: (generating) => set({ generating }),
+        tags: [],
+        setTags: (tags) => set({ tags }),
+        activeTag: initialState.activeTag,
+        setActiveTag: (tag) => set({ activeTag: tag }),
+        activeColor: initialState.activeColor,
+        setActiveColor: (color) => set({ activeColor: color }),
       }),
       { name: "images-store", storage: createJSONStorage(() => localStorage) }
     )

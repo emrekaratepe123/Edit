11319938bacd3be4caf0bb7d501cb22ccb00aae5
brunoDev100 <github_out1@outlook.com>
2024-09-16 10:36:04 +0530

    refactor: Editor component to include ImageTools and UploadForm

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import React from "react";
-import UploadImage from "./upload/UploadImage";
 import Layers from "./layers/Layers";
 import { ModeToggle } from "./theme/ModeToggle";
 import ActiveImage from "./ActiveImage";
+import UploadForm from "./upload/UploadForm";
+import { useLayerStore } from "@/lib/layer-store";
+import ImageTools from "./toolbar/ImageTools";
 
 function Editor() {
+  const activeLayer = useLayerStore((state) => state.activeLayer);
+
   return (
     <div className="flex h-full">
       <div className="py-6 px-4 min-w-48 ">
         <div className="pb-12 text-center">
           <ModeToggle />
         </div>
+        <div className="flex flex-col gap-4 ">
+          {activeLayer.resourceType === "image" ? <ImageTools /> : null}
+        </div>
       </div>
-      <UploadImage />
+      <UploadForm />
       <ActiveImage />
       <Layers />
     </div>

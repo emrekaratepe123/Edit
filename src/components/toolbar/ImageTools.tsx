d4a32762485b6885e25237613fc962949f4a873b
brunoDev100 <github_out1@outlook.com>
2024-09-19 14:57:57 +0530

    refactor: Add BgRemove component to ImageTools

--- src/components/toolbar/ImageTools.tsx
+++ src/components/toolbar/ImageTools.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import GenRemove from "./GenRemove";
+import BgRemove from "./BgRemove";
 
 function ImageTools() {
-  return <GenRemove />;
+  return (
+    <>
+      <GenRemove />
+      <BgRemove />
+    </>
+  );
 }
 
 export default ImageTools;

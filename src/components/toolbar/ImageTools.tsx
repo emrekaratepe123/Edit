27281d636bb4aca3d24ec23e9d7dd96751dc5679
brunoDev100 <github_out1@outlook.com>
2024-09-19 19:58:20 +0530

    feat: Add BgReplace component for background replacement functionality

--- src/components/toolbar/ImageTools.tsx
+++ src/components/toolbar/ImageTools.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GenRemove from "./GenRemove";
 import BgRemove from "./BgRemove";
+import BgReplace from "./BgReplace";
 
 function ImageTools() {
   return (
     <>
       <GenRemove />
       <BgRemove />
+      <BgReplace />
     </>
   );
 }

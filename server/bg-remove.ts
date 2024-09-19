2fd5082c6d93b1a423ee7ea1878e059bfac46d6a
brunoDev100 <github_out1@outlook.com>
2024-09-19 14:57:04 +0530

    feat: Add bgRemove function for background removal functionality

---
+++ server/bg-remove.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import checkImageProcessing from "@/lib/check-processing";
+import { actionClient } from "@/lib/safe-action";
+import { v2 as cloudinary } from "cloudinary";
+import { z } from "zod";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const bgRemoveSchema = z.object({
+  activeImage: z.string(),
+  format: z.string(),
+});
+
+export const bgRemove = actionClient
+  .schema(bgRemoveSchema)
+  .action(async ({ parsedInput: { activeImage, format } }) => {
+    const form = activeImage.split(format);
+    const pngConvert = form[0] + "png";
+    const parts = pngConvert.split("/upload/");
+    const removeUrl = `${parts[0]}/upload/e_background_removal/${parts[1]}`;
+
+    let isProcessed = false;
+    const maxAttempts = 20;
+    const delay = 1000;
+
+    for (let attempt = 0; attempt < maxAttempts; attempt++) {
+      isProcessed = await checkImageProcessing(removeUrl);
+      if (isProcessed) break;
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+
+    if (!isProcessed) throw new Error("image processing timeout out");
+    return { success: removeUrl };
+  });

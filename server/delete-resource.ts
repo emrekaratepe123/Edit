9d07752ad833d706249218d0bef8588d675ef54d
brunoDev100 <github_out1@outlook.com>
2024-09-22 23:12:31 +0530

    refactor: Add deleteResource function for deleting images and videos

---
+++ server/delete-resource.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { actionClient } from "@/lib/safe-action";
+import { v2 as cloudinary } from "cloudinary";
+import { z } from "zod";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const deleteSchema = z.object({
+  publicId: z.string(),
+  resourceType: z.enum(["image", "video"]).optional().default("image"),
+});
+
+export const deleteResource = actionClient
+  .schema(deleteSchema)
+  .action(async ({ parsedInput: { publicId, resourceType } }) => {
+    try {
+      const result = await new Promise((resolve, reject) => {
+        cloudinary.uploader.destroy(
+          publicId,
+          { resource_type: resourceType },
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+      });
+
+      return { success: result };
+    } catch (error) {
+      return {
+        error:
+          (error as Error).message ||
+          "An error occurred while deleting the resource",
+      };
+    }
+  });

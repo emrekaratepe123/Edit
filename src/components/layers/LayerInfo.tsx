bb681ad2fc5a9e54b31ab2e17ff2b82448eebe45
brunoDev100 <github_out1@outlook.com>
2024-09-22 16:49:51 +0530

    refactor: Update LayerInfo component UI and content layout

--- src/components/layers/LayerInfo.tsx
+++ src/components/layers/LayerInfo.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "../ui/button";
 import { Ellipsis, Trash } from "lucide-react";
 import { Layer, useLayerStore } from "@/lib/layer-store";
@@ -26,23 +19,21 @@ export default function LayerInfo({
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">
+        <Button variant="outline" className="border-0">
           <Ellipsis size={18} />
         </Button>
       </DialogTrigger>
-      <DialogContent className="text-xs">
-        <h3 className="text-lg font-medium text-center mb-2">
-          Layer {layer.id}
-        </h3>
-        <div className="py-4 space-y-0.5">
-          <p>
+      <DialogContent className="flex flex-col gap-6 justify-center">
+        <h3 className="text-[1.2rem] font-medium mt-2">Layer {layer.id}</h3>
+        <div className="flex flex-col gap-3">
+          <p className="text-[1rem]">
             <span className="font-bold">Filename:</span> {layer.name}
           </p>
-          <p>
+          <p className="text-[1rem]">
             <span className="font-bold">Format:</span> {layer.format}
           </p>
-          <p>
-            <span className="font-bold"> Size:</span> {layer.width}X
+          <p className="text-[1rem]">
+            <span className="font-bold"> Size:</span> {layer.width} X{" "}
             {layer.height}
           </p>
         </div>

7be82729e2ce68b13b2542eb24431be5882f507a
brunoDev100 <github_out1@outlook.com>
2024-12-08 11:09:50 +0530

    feat: Update NextAuth configuration to use Google provider with client ID and secret, and set custom sign-in page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,17 @@
+import { GOOGLE_CLIENT_SECRET } from "./constants/index";
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "../../prisma/prisma";
+import { GOOGLE_CLIENT_ID } from "./constants";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google],
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     authorized: async ({ auth }) => {
@@ -12,4 +19,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
   session: { strategy: "jwt" },
+  pages: {
+    signIn: "/auth",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });

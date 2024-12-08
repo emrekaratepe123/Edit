fc190d3dd0e0715cacd118cf66aaf9a10abcbfc0
brunoDev100 <github_out1@outlook.com>
2024-12-08 17:13:34 +0530

    feat: conditionally render toolbar tools based on active layer resource type

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -2,6 +2,7 @@ import { useLayerStore } from "@/lib/layer-store";
 import ImageTools from "./ImageTools";
 import VideoTools from "./VideoTools";
 import UserActions from "./UserActions";
+import { cn } from "@/lib/utils";
 
 function Toolbar() {
   const activeLayer = useLayerStore((state) => state.activeLayer);
@@ -9,7 +10,16 @@ function Toolbar() {
   return (
     <div className="p-4 flex flex-col gap-2">
       <UserActions />
-      <div className="flex flex-col gap-2 bg-background items-center rounded-2xl p-2 w-fit">
+      <div
+        className={cn(
+          "flex flex-col gap-2 bg-background items-center rounded-2xl p-2 w-fit",
+          {
+            hidden:
+              activeLayer.resourceType !== "image" &&
+              activeLayer.resourceType !== "video",
+          }
+        )}
+      >
         {activeLayer.resourceType === "image" ? <ImageTools /> : null}
         {activeLayer.resourceType === "video" ? <VideoTools /> : null}
       </div>

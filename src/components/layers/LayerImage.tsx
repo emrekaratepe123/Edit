3e795deeeba84aa36e89fe66d1526d0ac9cb40e9
brunoDev100 <github_out1@outlook.com>
2024-12-08 17:30:51 +0530

    feat: enhance LayerImage component with improved styling and placeholder image

--- src/components/layers/LayerImage.tsx
+++ src/components/layers/LayerImage.tsx
@@ -1,19 +1,21 @@
-"use-client";
-
-import React from "react";
 import Image from "next/image";
 import { Layer } from "@/lib/layer-store";
 
 function LayerImage({ layer }: { layer: Layer }) {
   if (layer.url)
     return (
-      <div className="w-12 h-12 flex items-center justify-center ">
+      <div className="w-16 h-16 flex items-center justify-center relative shrink-0">
         <Image
-          className="w-full object-contain h-full rounded-sm"
+          className="w-full object-contain h-full rounded-sm z-10"
           alt={"layer"}
           src={layer.format === "mp4" ? layer.poster || layer.url : layer.url}
-          width={50}
-          height={50}
+          fill
+        />
+        <Image
+          className="w-full object-cover h-full rounded-sm opacity-60"
+          alt={"layer"}
+          src="/layer-placeholder.jpg"
+          fill
         />
       </div>
     );

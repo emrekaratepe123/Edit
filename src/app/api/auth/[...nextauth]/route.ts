969f44ed2c39648fdabe41a20da1c66f06033b51
brunoDev100 <github_out1@outlook.com>
2024-12-15 01:23:04 +0530

    refactor: remove Google provider setup from NextAuth configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,3 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import { handlers } from "@/lib/auth";
-
-export const {
-  handlers: handler,
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-});
 
 export const { GET, POST } = handlers;

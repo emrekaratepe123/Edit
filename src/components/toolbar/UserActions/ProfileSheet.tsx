f69e1ae687cdeb2b5f52b10233ca4c56c5889800
brunoDev100 <github_out1@outlook.com>
2024-12-14 15:09:25 +0530

    feat: add ProfileSheet component for user profile display and management

---
+++ src/components/toolbar/UserActions/ProfileSheet.tsx
@@ -0,0 +1,91 @@
+import { Button } from "@/components/ui/button";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { User } from ".";
+import { Gem } from "lucide-react";
+
+interface ProfileSheetProps {
+  user: User;
+  name: string;
+  image: string;
+  email: string;
+  theme?: string;
+  handleSignOut: () => void;
+}
+
+const ProfileSheet = ({
+  user,
+  name,
+  email,
+  image,
+  theme,
+  handleSignOut,
+}: ProfileSheetProps) => {
+  return (
+    <SheetContent className="flex flex-col gap-6">
+      <SheetHeader>
+        <SheetTitle>
+          <Image
+            src={theme === "light" ? "/logo-full-black.svg" : "/logo-full.svg"}
+            width={120}
+            height={36}
+            className="rounded-full object-cover shrink-0 text-foreground"
+            alt="logo-full"
+          />
+        </SheetTitle>
+      </SheetHeader>
+      <div className="flex flex-col gap-4 flex-1 justify-between">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-4">
+            <Image
+              className="rounded-full"
+              src={image || "/default-avatar.png"}
+              width={38}
+              height={38}
+              alt="Avatar"
+            />
+            <div className="flex flex-col">
+              <h3>{name}</h3>
+              <p className="text-neutral-400 text-sm">{email}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="flex flex-col gap-4">
+            <div className="h-2 bg-secondary rounded-full">
+              <div
+                style={{ width: `${(user?.credits / 20) * 100}%` }}
+                className="h-full rounded-[inherit] bg-[#f67127] transition-all duration-200 ease-in"
+              />
+            </div>
+            <div className="flex justify-between items-center text-sm -mt-2">
+              <p>Credits Remaining</p>
+              <p>{user?.credits} / 20</p>
+            </div>
+            <div className="rounded-lg border p-4 px-6 flex flex-col justify-center items-center gap-3">
+              <p className="text-sm">
+                You have {user?.credits} credits remaining!
+              </p>
+              <p className="text-xs -mt-2 text-neutral-400">
+                Upgrade to gain more credits
+              </p>
+              <Button className="font-semibold w-full gap-2 bg-[#f67127] text-white">
+                <Gem className="h-5 w-5" />
+                Upgrade
+              </Button>
+            </div>
+          </div>
+        </div>
+        <Button onClick={handleSignOut} className="font-semibold">
+          Log Out
+        </Button>
+      </div>
+    </SheetContent>
+  );
+};
+
+export default ProfileSheet;

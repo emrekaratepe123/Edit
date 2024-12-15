683c2f4999061f29de67012e2d634a7ffc7aabdd
brunoDev100 <github_out1@outlook.com>
2024-12-15 14:49:27 +0530

    refactor: enhance responsiveness of LampContainer by adjusting dimensions based on device type

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { useResponsive } from "@/lib/hooks/useResponsive";
 
 export default function LampDemo() {
   return (
@@ -29,6 +30,8 @@ export const LampContainer = ({
   children: React.ReactNode;
   className?: string;
 }) => {
+  const isMobile = useResponsive();
+
   return (
     <div
       className={cn(
@@ -38,8 +41,8 @@ export const LampContainer = ({
     >
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
         <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "30rem" }}
+          initial={{ opacity: 0.5, width: isMobile ? "4rem" : "15rem" }}
+          whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
           transition={{
             delay: 0.3,
             duration: 0.8,
@@ -48,14 +51,14 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[15rem] sm:w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
         >
           <div className="absolute  w-[100%] left-0 bg-background h-40 bottom-0 z-10 [mask-image:linear-gradient(to_top,white,transparent)]" />
           <div className="absolute  w-40 h-[100%] left-0 bg-background  bottom-0 z-10 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "30rem" }}
+          initial={{ opacity: 0.5, width: isMobile ? "4rem" : "15rem" }}
+          whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
           transition={{
             delay: 0.3,
             duration: 0.8,
@@ -64,7 +67,7 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
+          className="absolute inset-auto left-1/2 h-56 w-[15rem] sm:w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
         >
           <div className="absolute  w-40 h-[100%] right-0 bg-background  bottom-0 z-10 [mask-image:linear-gradient(to_left,white,transparent)]" />
           <div className="absolute  w-[100%] right-0 bg-background h-40 bottom-0 z-10 [mask-image:linear-gradient(to_top,white,transparent)]" />
@@ -83,14 +86,14 @@ export const LampContainer = ({
           className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
         ></motion.div>
         <motion.div
-          initial={{ width: "15rem" }}
-          whileInView={{ width: "30rem" }}
+          initial={{ width: isMobile ? "4rem" : "15rem" }}
+          whileInView={{ opacity: 1, width: isMobile ? "15rem" : "30rem" }}
           transition={{
             delay: 0.3,
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "
+          className="absolute inset-auto z-50 h-0.5 w-[15rem] sm:w-[30rem] -translate-y-[7rem] bg-cyan-400 "
         ></motion.div>
 
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-background "></div>

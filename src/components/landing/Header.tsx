686faf59ad94d96e4be13a6125b30b1ab7bffd0e
brunoDev100 <github_out1@outlook.com>
2024-12-15 17:36:08 +0530

    refactor: update Header component for improved layout and responsiveness

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { HoverBorderGradient } from "../ui/hover-border-gradient";
 import Logo from "../Logo";
 import { useSession } from "next-auth/react";
 import ShineBorder from "../ui/shine-border";
@@ -11,10 +10,10 @@ export default function Header() {
   const { data: session } = useSession();
 
   return (
-    <header className="sticky top-8 w-[80%] mx-auto z-50 rounded-full">
+    <header className="fixed sm:sticky top-8 sm:left-0 left-1/2 -translate-x-1/2 sm:translate-x-0 w-[90%] sm:w-[80%] mx-auto z-50 rounded-full">
       <ShineBorder
         borderRadius={999999}
-        className="bg-gradient-to-r from-black via-gray-900 to-black text-white w-full justify-center flex flex-row items-center rounded-full px-12 py-4"
+        className="bg-gradient-to-r from-black via-gray-900 to-black text-white w-full justify-center flex flex-row items-center rounded-full px-8 sm:px-12"
         color={["#7c9ffe", "#FE8FB5", "#FFBE7B"]}
       >
         <Logo className="flex-1" />

b1983e75683f31b524c6ae2ee412066878b4bc89
brunoDev100 <github_out1@outlook.com>
2024-12-15 19:38:02 +0530

    refactor: enhance middleware to skip public and API routes for authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,34 @@ const imageExtensions = [".svg", ".jpg", ".png", ".webp", ".ico"];
 export default auth((req) => {
   const path = req.nextUrl.pathname;
 
-  // Skip middleware for image files
-  if (imageExtensions.some((ext) => path.endsWith(ext))) {
+  // Skip middleware for image files and public routes
+  if (
+    imageExtensions.some((ext) => path.endsWith(ext)) ||
+    publicRoutes.includes(path)
+  ) {
     return;
   }
 
-  const isPublicRoute = publicRoutes.includes(path);
+  // Skip middleware for API routes
+  if (path.startsWith("/api/")) {
+    return;
+  }
 
-  if (!req.auth && !isPublicRoute) {
+  if (!req.auth) {
     const newUrl = new URL("/auth", req.nextUrl.origin);
     return NextResponse.redirect(newUrl);
   }
 });
 
 export const config = {
-  matcher: ["/((?!_next).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };

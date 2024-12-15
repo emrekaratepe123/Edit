68d04bc843bb678f2fceb2907ccfb2f465c552c4
brunoDev100 <github_out1@outlook.com>
2024-12-15 17:36:25 +0530

    style: adjust layout and spacing in Hero component for improved visual hierarchy

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
 import { LampContainer } from "../ui/lamp";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Particles from "../ui/particles";
 import Link from "next/link";
-import AnimatedShinyText from "../ui/animated-shiny-text";
 import AnimatedGradientText from "../ui/animated-gradient-text";
 import { cn } from "@/lib/utils";
 
@@ -21,15 +19,15 @@ export default function Hero() {
   ];
 
   return (
-    <section className="pt-8 text-center relative">
-      <LampContainer>
+    <section className="text-center relative">
+      <LampContainer className="pt-10 sm:pt-0">
         <motion.div
-          className="mt-8 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-center font-medium tracking-tight text-transparent"
+          className="sm:mt-8 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-center font-medium tracking-tight text-transparent"
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          <h1 className="mb-3 md:mb-8 mt-8 flex justify-center items-center">
+          <h1 className="mb-3 md:mb-8 mt-0 sm:mt-8 flex justify-center items-center">
             <TypewriterEffectSmooth words={words} className="font-bold" />
           </h1>
           <p className="mx-auto max-w-[70%] text-wrap sm:max-w-[700px] text-gray-400 text-md sm:text-lg md:text-xl tracking-normal">
@@ -44,7 +42,7 @@ export default function Hero() {
               <AnimatedGradientText className="bg-white rounded-[inherit]">
                 <span
                   className={cn(
-                    `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent rounded-[inherit] px-4 py-1 text-lg`
+                    `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent rounded-[inherit] px-4 py-1 text-md sm:text-lg`
                   )}
                 >
                   Get Started
@@ -54,7 +52,7 @@ export default function Hero() {
           </div>
         </motion.div>
       </LampContainer>
-      <div className="relative w-[350px] h-[160px] sm:w-[400px] sm:h-[180px] md:w-[600px] md:h-[280px] lg:w-[900px] lg:h-[420px] mx-auto -translate-y-44 z-10 rounded-2xl border-2 border-gray-400 box-content">
+      <div className="relative w-[350px] h-[160px] sm:w-[400px] sm:h-[180px] md:w-[600px] md:h-[280px] lg:w-[900px] lg:h-[420px] mx-auto -translate-y-64 sm:-translate-y-44 z-10 rounded-2xl border-2 border-gray-400 box-content">
         <Image
           src="https://res.cloudinary.com/dqiqi75hm/image/upload/v1734187202/quickedit/dp6y8s8dstqfpq7svmj5.png"
           alt="Hero Image"

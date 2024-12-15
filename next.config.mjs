d2d67f522ee857da638dfcd76c9eacf0f7842931
brunoDev100 <github_out1@outlook.com>
2024-12-15 19:37:34 +0530

    refactor: remove SVG handling from webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -17,20 +17,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        // test: /\.(js|ts)x?$/,
-        // for webpack 5 use
-        and: [/\.(js|ts)x?$/],
-      },
-
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;

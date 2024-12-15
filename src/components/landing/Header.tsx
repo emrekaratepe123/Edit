ed0586f386f56a2162ae70375db36377fab944ea
brunoDev100 <github_out1@outlook.com>
2024-12-15 18:03:25 +0530

    refactor: enhance Header component for mobile responsiveness and adjust button styles

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -5,9 +5,12 @@ import Link from "next/link";
 import Logo from "../Logo";
 import { useSession } from "next-auth/react";
 import ShineBorder from "../ui/shine-border";
+import Image from "next/image";
+import { useResponsive } from "@/lib/hooks/useResponsive";
 
 export default function Header() {
   const { data: session } = useSession();
+  const isMobile = useResponsive();
 
   return (
     <header className="fixed sm:sticky top-8 sm:left-0 left-1/2 -translate-x-1/2 sm:translate-x-0 w-[90%] sm:w-[80%] mx-auto z-50 rounded-full">
@@ -16,36 +19,54 @@ export default function Header() {
         className="bg-gradient-to-r from-black via-gray-900 to-black text-white w-full justify-center flex flex-row items-center rounded-full px-8 sm:px-12"
         color={["#7c9ffe", "#FE8FB5", "#FFBE7B"]}
       >
-        <Logo className="flex-1" />
+        {isMobile ? (
+          <Link href="/">
+            <Image
+              src={"/images/logo.svg"}
+              width={30}
+              height={30}
+              className="object-cover shrink-0"
+              alt="logo-full"
+            />
+          </Link>
+        ) : (
+          <Logo className="shrink-0" />
+        )}
+
         <nav className="space-x-6 flex-1 flex justify-center">
           <Link
             href="#features"
-            className="text-md font-medium text-muted-foreground hover:text-primary transition-all duration-200 ease-in"
+            className="text-sm sm:text-[16px] font-medium text-muted-foreground hover:text-primary transition-all duration-200 ease-in"
           >
             Features
           </Link>
           <Link
             href="#pricing"
-            className="text-md font-medium text-muted-foreground hover:text-primary transition-all duration-200 ease-in"
+            className="text-sm sm:text-[16px] font-medium text-muted-foreground hover:text-primary transition-all duration-200 ease-in"
           >
             Pricing
           </Link>
         </nav>
-        <div className="flex items-center justify-end space-x-4 flex-1">
-          {session ? (
-            <Link href="/editor">
-              <Button className="px-6 rounded-lg" size="sm">
-                Go to Editor
-              </Button>
-            </Link>
-          ) : (
-            <Link href="/auth">
-              <Button className="px-6 rounded-lg" size="sm">
-                Sign In
-              </Button>
-            </Link>
-          )}
-        </div>
+
+        {session ? (
+          <Link href="/editor">
+            <Button
+              className="px-3 h-8 text-xs sm:text-sm sm:h-9 sm:px-6 rounded-lg"
+              size="sm"
+            >
+              Editor
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/auth">
+            <Button
+              className="px-3 h-8 text-xs sm:text-sm sm:h-9 sm:px-6 rounded-lg"
+              size="sm"
+            >
+              Sign In
+            </Button>
+          </Link>
+        )}
       </ShineBorder>
     </header>
   );

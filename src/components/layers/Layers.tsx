93b73c8518d3bb7732b7633690dd953c2908a613
brunoDev100 <github_out1@outlook.com>
2024-12-17 12:06:38 +0530

    feat: fetch layers on component mount and improve empty state display

--- src/components/layers/Layers.tsx
+++ src/components/layers/Layers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useImageStore } from "@/lib/image-store";
-import { useLayerStore } from "@/lib/layer-store";
+import { Layer, useLayerStore } from "@/lib/layer-store";
 import {
   Card,
   CardContent,
@@ -9,7 +9,13 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
-import { ArrowRight, Images, Layers2, PanelRightClose } from "lucide-react";
+import {
+  ArrowRight,
+  Frown,
+  Images,
+  Layers2,
+  PanelRightClose,
+} from "lucide-react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import LayerImage from "./LayerImage";
@@ -23,6 +29,7 @@ import {
   TooltipTrigger,
 } from "../ui/tooltip";
 import { useResponsive } from "@/lib/hooks/useResponsive";
+import getLayers from "../../../server/get-layers";
 
 export default function Layers() {
   const isMobile = useResponsive(1024);
@@ -31,6 +38,14 @@ export default function Layers() {
   useEffect(() => {
     setIsOpen(isMobile ? false : true);
   }, [isMobile]);
+
+  useEffect(() => {
+    const fetchLayers = async () => {
+      const { layers } = await getLayers();
+      setLayers(layers as Layer[]);
+    };
+    fetchLayers();
+  }, []);
 
   const {
     layers,
@@ -42,6 +57,7 @@ export default function Layers() {
     comparedLayers,
     toggleComparedLayer,
     setComparedLayers,
+    setLayers,
   } = useLayerStore((state) => ({
     layers: state.layers,
     activeLayer: state.activeLayer,
@@ -52,6 +68,7 @@ export default function Layers() {
     comparedLayers: state.comparedLayers,
     toggleComparedLayer: state.toggleComparedLayer,
     setComparedLayers: state.setComparedLayers,
+    setLayers: state.setLayers,
   }));
   const { generating } = useImageStore((state) => ({
     generating: state.generating,
@@ -87,6 +104,56 @@ export default function Layers() {
     setActiveLayer(newLayerId);
   };
 
+  const layersEle =
+    visibleLayers.length === 0 ? (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Frown />
+        <p className="text-sm text-secondary-foreground mt-2 text-center">
+          No layers to show
+          <br />
+          Add a new layer to get started
+        </p>
+      </div>
+    ) : (
+      visibleLayers.map((layer, index) => (
+        <div
+          className={cn(
+            "cursor-pointer ease-in-out hover:bg-secondary border border-transparent w-full p-1 rounded-sm",
+            {
+              "animate-pulse": generating,
+              "border-primary": layerComparisonMode
+                ? comparedLayers.includes(layer.id)
+                : activeLayer.id === layer.id,
+            }
+          )}
+          key={layer.id}
+          onClick={() => {
+            if (generating) return;
+            if (layerComparisonMode) toggleComparedLayer(layer.id);
+            else setActiveLayer(layer.id);
+          }}
+        >
+          <div className="flex w-full gap-4 items-center justify-between">
+            {!layer.url ? (
+              <p className="text-xs font-medium justify-self-end pl-2">
+                New Layer
+              </p>
+            ) : null}
+            <LayerImage layer={layer} className="w-14 h-14" />
+            {layer.url && (
+              <div className="flex-1 flex flex-col gap-1 w-min overflow-x-hidden">
+                <p className="text-xs overflow-hidden text-wrap">{`${layer.name}.${layer.format}`}</p>
+                <p className="text-[0.7rem] overflow-hidden text-wrap text-[#94a3b8]">
+                  {layer.width} X {layer.height}
+                </p>
+              </div>
+            )}
+            <LayerInfo layer={layer} layerIndex={index} />
+          </div>
+        </div>
+      ))
+    );
+
   return (
     <>
       <Button
@@ -104,7 +171,7 @@ export default function Layers() {
 
       <Card
         className={cn(
-          "fixed lg:relative right-0 top-0 h-[calc(100vh-2rem)] lg:h-auto basis-[360px] my-4 mr-4 shrink-0 overflow-y-auto overflow-x-hidden flex flex-col shadow-2xl rounded-2xl transition-transform duration-300 ease-in-out z-40",
+          "fixed lg:relative right-0 top-0 h-[calc(100svh-2rem)] lg:h-auto basis-[360px] my-4 mr-4 shrink-0 overflow-y-auto overflow-x-hidden flex flex-col shadow-2xl rounded-2xl transition-transform duration-300 ease-in-out z-40",
           {
             "translate-x-0": isOpen,
             "translate-x-[calc(100%+20px)]": !isOpen,
@@ -150,43 +217,7 @@ export default function Layers() {
         </CardHeader>
 
         <CardContent className="flex-1 flex flex-col gap-2 w-full">
-          {visibleLayers.map((layer, index) => (
-            <div
-              className={cn(
-                "cursor-pointer ease-in-out hover:bg-secondary border border-transparent w-full p-1 rounded-sm",
-                {
-                  "animate-pulse": generating,
-                  "border-primary": layerComparisonMode
-                    ? comparedLayers.includes(layer.id)
-                    : activeLayer.id === layer.id,
-                }
-              )}
-              key={layer.id}
-              onClick={() => {
-                if (generating) return;
-                if (layerComparisonMode) toggleComparedLayer(layer.id);
-                else setActiveLayer(layer.id);
-              }}
-            >
-              <div className="flex w-full gap-4 items-center justify-between">
-                {!layer.url ? (
-                  <p className="text-xs font-medium justify-self-end pl-2">
-                    New Layer
-                  </p>
-                ) : null}
-                <LayerImage layer={layer} className="w-14 h-14" />
-                {layer.url && (
-                  <div className="flex-1 flex flex-col gap-1 w-min overflow-x-hidden">
-                    <p className="text-xs overflow-hidden text-wrap">{`${layer.name}.${layer.format}`}</p>
-                    <p className="text-[0.7rem] overflow-hidden text-wrap text-[#94a3b8]">
-                      {layer.width} X {layer.height}
-                    </p>
-                  </div>
-                )}
-                <LayerInfo layer={layer} layerIndex={index} />
-              </div>
-            </div>
-          ))}
+          {layersEle}
         </CardContent>
 
         <CardContent className="sticky bottom-0 bg-card flex gap-2 shrink-0 p-4 z-20">

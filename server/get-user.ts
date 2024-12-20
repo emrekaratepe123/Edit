7eef5c0d52e4bbe165b7c9d7339b3b157165c3e3
brunoDev100 <github_out1@outlook.com>
2024-12-20 10:00:39 +0530

    refactor: streamline user fetching logic in getUser function

--- server/get-user.ts
+++ server/get-user.ts
@@ -3,20 +3,13 @@
 import { prisma } from "../prisma/prisma";
 
 const getUser = async (email: string) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    return user;
-  } catch (error) {
-    console.error("Error in fetching user:", error);
-    return {
-      error: "Error in fetching user: " + String((error as any).message),
-    };
-  }
+  return user;
 };
 
 export default getUser;
